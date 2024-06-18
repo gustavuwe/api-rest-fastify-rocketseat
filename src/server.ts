dd9bd4cd773d677b11033f419630aa58e598f728
Gustav Uwe <gustavuwe.123@gmail.com>
2024-06-17 21:22:35 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+import { env } from '../src/env/index';
+import { transactionsRoutes } from './routes/transactions'
+
+const app = fastify()
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
